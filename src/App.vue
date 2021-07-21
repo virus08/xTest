2e9a5fb221bf8560f3fe79114f4739c8bdaaf41c
virus08 <fuangmali@gmail.com>
2021-07-21 10:54:27 +0700

    save

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,78 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar app color="white" flat>
+      <v-row align="center" justify="space-around">
+        <v-container class="py-0 fill-height">
+          <v-avatar size="50">
+            <v-img :src="img"></v-img>
+          </v-avatar>
+
+          <h1 class="blue--text text--darken-4">VSTECS</h1>
+          <v-spacer />
+          <v-col v-for="link in links" :key="link">
+            <v-btn
+              @click="clickmenu(link)"
+              depressed
+              block
+              color="blue-grey"
+              dark
+              x-large
+            >
+              {{ link }}
+            </v-btn>
+          </v-col>
+          <v-spacer></v-spacer>
+          {{ user.name }}
+        </v-container>
+      </v-row>
+    </v-app-bar>
+
+    <v-main  >
+      <router-view />
+    </v-main>
+    <v-footer app color="blue-grey" class="white--text">
+      <span>VST ECS (Thailand) Co.,Ltd.</span>
+      <v-spacer />
+      <span>&copy; 2021 (version 0.0.3)</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+import { msalMixin } from "vue-msal";
+ 
+export default {
+  mixins: [msalMixin],
+  data: () => ({
+    img: process.env.VUE_APP_REDIRECT + "/vstecs.png",
+    links: ["Dashboard", "Query"],
+  }),
+  methods: {
+    async clickmenu(item) {
+       /*
+      if (item != this.$route.name) {
+        this.$router.push({ name: item }).go;
+      }*/
+    },
+  },
+  computed: {
+    user() {
+      let user = {};
+      if (this.msal.isAuthenticated) {
+        this.msal.graph.callAfterInit = true;
+        
+        user = {
+          ...this.msal.user,
+          profile: {},
+        };
+        if (this.msal.graph && this.msal.graph.profile) {
+          user.profile = this.msal.graph.profile;
+        }
+      } else {
+        this.$msal.signIn();
+      }
+      return user;
+    },
+  },
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,22 @@
 import Vue from 'vue'
+import './plugins/axios'
 import App from './App.vue'
-import vuetify from './plugins/vuetify'
+import vuetify from './plugins/vuetify';
 import router from './router'
 
-Vue.config.productionTip = false
+import msal from 'vue-msal' // For Authentication Module
 
+Vue.config.productionTip = false 
+
+// For Authentication Module
+Vue.use(msal, {
+  auth: {
+    clientId: process.env.VUE_APP_CLIENT_ID,
+    tenantId: process.env.VUE_APP_TENANT,
+    redirectUri: process.env.VUE_APP_REDIRECT,
+    scopes: process.env.VUE_APP_XSCOPE
+  }
+});
 new Vue({
   vuetify,
   router,
